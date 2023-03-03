b5af985d46fee9a0f0f98648dbf678b77117993d
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-03-03 19:15:11 +0530

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 function Register(event){
-    // alert('Working')
-    event.preventDefault()    
+
+    event.preventDefault();
+    // alert("Working");
 
     var UserName = document.getElementById("UserName").value;
     var UserNumber = document.getElementById("UserNumber").value;
@@ -11,40 +12,74 @@ function Register(event){
     var Data = {Name: UserName, Number: UserNumber, Email: UserEmail, Password: UserPassword, ConfirmPassword: UserConfirmPassword}
 
     var DataFromLS = JSON.parse(localStorage.getItem("DataList")) || [];
-   
+
     var flag = false;
 
-   for(var i=0; i < DataFromLS.length; i++){
-    if(DataFromLS[i].Email === UserEmail)
-       flag = true;
+    for(var i=0; i < DataFromLS.length; i++){
+        if(DataFromLS[i].Email === UserEmail){
+        flag =  true;
+       }
+     }
+
+     if(flag === true){
+        alert("Email is Already Present");
+     }
+     else if (UserPassword.length < 1 && UserName.length < 1 && UserEmail.length < 1 && UserNumber.length < 1 && UserConfirmPassword.length < 1) {
+         alert("must all field");
+     }
+     else if(UserPassword < 8){
+        alert("Password must have 8 digit");   
+     }
+     else if(UserPassword != UserConfirmPassword){
+        alert("Password Mismatch")
+     }    
+     else{
+        DataFromLS.push(Data);
+        localStorage.setItem("DataList", JSON.stringify(DataFromLS));
+        document.getElementById("UserName").value = '';
+        document.getElementById("UserNumber").value = '';
+        document.getElementById("UserEmail").value = '';
+        document.getElementById("UserPassword").value = '';
+        document.getElementById("UserConfirmPassword").value = '';
+        alert("Registration DOne");
+        console.log(DataFromLS, "DataFromLS")
+    
+     }
+ 
+}
+
+
+function login(event){
+
+    event.preventDefault();
+    // alert("working");
+
+    var UserEmail = document.getElementById("UserEmail").value;
+    var UserPassword = document.getElementById("UserPassword").value;
+
+    var DataFromLS = JSON.parse(localStorage.getItem("DataList"));
+    console.log(DataFromLS, "DataFromLS");
+
+    var flag = false;
+
+    for(var i = 0; i < DataFromLS.length; i++ ){
+        if(DataFromLS[i].Email === UserEmail && DataFromLS[i].Password === UserPassword){
+            flag = true;
+        }
     }
 
     if(flag === true){
-    alert("Email is Already Present")
+
+     document.getElementById("UserEmail").value = '';
+     document.getElementById("UserPassword").value = '';
+     window.location.href = "/index.html";
+     alert("login successfully");
+     
     }
-    else if (UserPassword.length < 1 && UserName.length < 1 && UserEmail.length < 1 
-        && UserNumber.length < 1 && UserConfirmPassword.length < 1) {
-            alert("must fill all field")
+    else{
+        alert("check your credentials or please create your account");
     }
-            else if (UserPassword.length < 8) {
-            alert("password is atleast 8 digit")
-        }
-        else if(UserPassword != UserConfirmPassword){
-            alert("Password Mismatch")
-        }
-        else
-         {
-            DataFromLS.push(Data);
-            localStorage.setItem("DataList", JSON.stringify(DataFromLS));
-            // console.log(DataFromLS, "DataFromLS");
-            document.getElementById("UserName").value = '';
-            document.getElementById("UserNumber").value = '';
-            document.getElementById("UserEmail").value = '';
-            document.getElementById("UserPassword").value = '';
-            document.getElementById("UserConfirmPassword").value = '';
-            alert("registration done");
-        }
-        
+
 }
 
 
@@ -57,9 +92,30 @@ function Register(event){
 
 
 
-// function Register(event) {
 
-//     event.preventDefault()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Register(event){
+//     // alert('Working')
+//     event.preventDefault()    
 
 //     var UserName = document.getElementById("UserName").value;
 //     var UserNumber = document.getElementById("UserNumber").value;
@@ -67,46 +123,41 @@ function Register(event){
 //     var UserPassword = document.getElementById("UserPassword").value;
 //     var UserConfirmPassword = document.getElementById("UserConfirmPassword").value;
 
-//     var Data = { Name: UserName, Number: UserNumber, Email: UserEmail, Password: UserPassword, confirmPassword: UserConfirmPassword }
-//     console.log(Data, "Data")
-
+//     var Data = {Name: UserName, Number: UserNumber, Email: UserEmail, Password: UserPassword, ConfirmPassword: UserConfirmPassword}
 
 //     var DataFromLS = JSON.parse(localStorage.getItem("DataList")) || [];
+   
+//     var flag = false;
 
-//     flag = false;
-
-//     for (i = 0; i < DataFromLS.length; i++) {
-//         if(DataFromLS[i].Email === UserEmail);
-//         flag = true
+//    for(var i=0; i < DataFromLS.length; i++){
+//     if(DataFromLS[i].Email === UserEmail)
+//        flag = true;
 //     }
 
-//     if (flag === true) {
-//         alert("Email is already present")
+//     if(flag === true){
+//     alert("Email is Already Present")
 //     }
-//     else if (UserName.length < 1 && UserNumber.length < 1 && UserEmail.length < 1 && UserPassword.length < 1 && UserConfirmPassword.length) {
-//         alert("All Field Must Be Filled !")
+//     else if (UserPassword.length < 1 && UserName.length < 1 && UserEmail.length < 1 
+//         && UserNumber.length < 1 && UserConfirmPassword.length < 1) {
+//             alert("must fill all field")
 //     }
-//     else if (UserPassword.length < 8) {
-//         alert("password must be 8 digit")
-//     }
-//     else if (UserPassword != UserConfirmPassword) {
-//         alert("Password Mismatch")
-//     }
-//     else {
-//         DataFromLS.push(Data);
-//         localStorage.setItem("DataList", JSON.stringify(DataFromLS));
-//         console.log(DataFromLS, "DataFromLS");
-//         document.getElementById("UserName").value = '';
-//         document.getElementById("UserNumber").value = '';
-//         document.getElementById("UserEmail").value = '';
-//         document.getElementById("UserPassword").value = '';
-//         document.getElementById("UserConfirmPassword").value = '';
-//         alert('Registration Done');
-
-
-//     }
-
+//             else if (UserPassword.length < 8) {
+//             alert("password is atleast 8 digit")
+//         }
+//         else if(UserPassword != UserConfirmPassword){
+//             alert("Password Mismatch")
+//         }
+//         else
+//          {
+//             DataFromLS.push(Data);
+//             localStorage.setItem("DataList", JSON.stringify(DataFromLS));
+//             // console.log(DataFromLS, "DataFromLS");
+//             document.getElementById("UserName").value = '';
+//             document.getElementById("UserNumber").value = '';
+//             document.getElementById("UserEmail").value = '';
+//             document.getElementById("UserPassword").value = '';
+//             document.getElementById("UserConfirmPassword").value = '';
+//             alert("registration done");
+//         }
+        
 // }
-
-
-
